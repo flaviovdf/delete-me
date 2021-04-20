7dfdf355ec909e63e4aba53e6f6759377dd75eaa
Flavio Figueiredo <flaviovdf@gmail.com>
2021-04-20 18:18:49 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf8
+
+
+from manga_search import linsearch
+
+
+def main():
+    data = [8, 7, 2, 1, 2, 0]
+    print(linsearch(data, 8))
+    
+    
+if __name__ == '__main__':
+    main()
